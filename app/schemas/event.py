e44fb1989c04873495ed27b26cfa114aa7f3ebf1
anril <dlyabsinetolko@gmail.com>
2026-01-08 09:20:14 +0300

    Added postgres into docker compose

--- app/schemas/event.py
+++ app/schemas/event.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel
 from datetime import datetime
 
+class Metadata(BaseModel):
+    page: str
+    device_type: str
 
 class Event(BaseModel):
     id: int | None = None
     event_type: str
     user_id: str
     timestamp: datetime
-    metadata: dict
+    metadata: Metadata
